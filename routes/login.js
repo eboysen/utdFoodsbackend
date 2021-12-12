1fe8eefd77c89e325ab591681eef539355d9f9e1
Eric Boysen <ericboysen@outlook.com>
2021-12-12 10:16:10 -0600

    login

--- routes/login.js
+++ routes/login.js
@@ -7,11 +7,27 @@ import passportLocal from "passport-local";
 
 const router = express.Router();
 
-const login = router.post('/login',passport.authenticate('local',{
-    successRedirect:'/',
-    failureRedirect:'/login',
-    failureFlash:true
-}));
+const login = router.post('/login',function (req,res,next){
+    pass.authenticate('local', (err,user,info)=>{
+    if(err)
+        return next(err);
+    if(user==false){
+        res.status(200).json('Username or password are incorrect');
+    }
+    else{
+        req.login(user,function(err){
+            if(err)
+                return next(err);
+            return res.status(200).json(req.user);
+        });
+    }
+    console.log(req.sessionID);
+    console.log(req.session);
+    //return res.redirect('/');
+    
+    console.log(req.user);
+
+})(req,res,next)});
 
 export default login;
 /*
