cc6886dd8106bcbc7ddad5efeaae775360f35fbd
Eric Boysen <ericboysen@outlook.com>
2021-12-11 20:12:02 -0600

    connect

--- db/index.js
+++ db/index.js
@@ -1,11 +1,12 @@
 import pg from "pg";
-export const pool = new pg.Pool({
-    user:"quqktpmt",
-    password:"AAsjtrHgPmF5YbSUklXk_cVEwn3J3Zu7",
-    database: "quqktpmt",
+/*export const pool = new pg.Pool({
+    user:"me",
+    password:"password",
+    database: "recipes",
     port: "5432",
-    host: "castor.db.elephantsql.com "
-});
+    host: "localhost",
+});*/
+ export const pool = new pg.Client("postgres://quqktpmt:AAsjtrHgPmF5YbSUklXk_cVEwn3J3Zu7@castor.db.elephantsql.com/quqktpmt");
 
 export const db = {
     query: (text,params,callback)=>{
