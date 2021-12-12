18253fcab602e84a7f7c8adfdc8d3fe3fcee5e07
Eric Boysen <ericboysen@outlook.com>
2021-12-11 18:00:42 -0600

    cors

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const routesArray = ['/','/recipe/*','/fiveNew'];
 init(pass);
 //CORS Header
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://utd-foods.web.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
