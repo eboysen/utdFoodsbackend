904b2df7cd6fd66e06dfb9d01b74b57da711da4e
Eric Boysen <ericboysen@outlook.com>
2021-12-12 16:01:47 -0600

    cookie

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ init(pass);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://utd-foods.web.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", "false");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     next();
   });
@@ -114,6 +114,7 @@ app.post('/login',function (req,res,next){
             if(err)
                 return next(err);
             console.log("USER AT LOG"+req.user);
+            res.cookie('id',req.sessionID);
             return res.status(200).json(req.user);
         });
     }
