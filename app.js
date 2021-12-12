9f5b2a0bce6ea7308d4775fa789320112b86131f
Eric Boysen <ericboysen@outlook.com>
2021-12-12 15:58:28 -0600

    cred

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ init(pass);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://utd-foods.web.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Credentials", "false");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
     next();
   });
